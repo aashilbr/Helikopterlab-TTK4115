627d08dc755e25d50be7da419b13d5bde90e7778
Åshild Rosland <59867493+aashilbr@users.noreply.github.com>
2020-11-20 21:17:52 +0100

    Removed uneccessary comments

--- Part 3 - Luenberger observer/init_heli_3_10.m
+++ Part 3 - Luenberger observer/init_heli_3_10.m
@@ -3,9 +3,9 @@
 % the course TTK4115. Run this file before you execute QuaRC_ -> Build 
 % to build the file heli_q8.mdl.
 
-% Oppdatert høsten 2006 av Jostein Bakkeheim
-% Oppdatert høsten 2008 av Arnfinn Aas Eielsen
-% Oppdatert høsten 2009 av Jonathan Ronen
+% Oppdatert hÃ¸sten 2006 av Jostein Bakkeheim
+% Oppdatert hÃ¸sten 2008 av Arnfinn Aas Eielsen
+% Oppdatert hÃ¸sten 2009 av Jonathan Ronen
 % Updated fall 2010, Dominik Breu
 % Updated fall 2013, Mark Haring
 % Updated spring 2015, Mark Haring
@@ -39,29 +39,18 @@ K_2 = L_3/J_e;
 K_3 = - (L_4*L_2)/(L_3*J_lambda);
 
 J_p = 2*m_p*(l_p)^2
-%SJEKK DEVICE MANAGER -> COMX HVER GANG!!!
+
+%Device manager
 PORT = 7;
 
-%IMU OFFSET
+% Offset from the IMU
 IMU_offset = [0.018;
               -0.005;
               -0.09;
               0.01;
               0.004];
-          
-% IMU_offset = [-0.07;
-%               0.0052;
-%               0.045;
-%               0;
-%              -0.003];
 
-
-% IMU_offset = [0;
-%               -0.005;
-%               -0.085;
-%               0.01;
-%               0.003];
-%DEFINE MATRICES
+% System matrices
  A = [ 0 1 0 0 0 ;
        0 0 0 0 0 ;
        0 0 0 0 0 ;
@@ -90,15 +79,7 @@ C = [1 0 0 0 0;
     0 0 0 1 0;
     0 0 0 0 1];
 
-% Q = [125 0 0 0 0;
-%       0 80 0 0 0;
-%       0 0 150 0 0;
-%       0 0 0 1 0;
-%       0 0 0 0 1 ];
-%    
-%  R = [ 1.7 0 ;
-%       0 0.5 ];
-
+% LQR method
 Q = [125 0 0 0 0;
      0 80 0 0 0;
      0 0 150 0 0;
@@ -109,21 +90,10 @@ Q = [125 0 0 0 0;
  K = lqr(A,B,Q,R);
  F = [ K(1,1) K(1,3);
          K(2,1) K(2,3) ];
+ % Eigenvalues of the controller
  P = eig(A-B*K);
-%p = [-3 -1-1i -1+1i -2 -2];
- %p = [-1 -0.9396+0.3420i -0.9396-0.3420i -0.7660-0.6428i -0.7660+0.6428i];
- %p = [-1 -1+0.5i -1-0.5i 0-1i 0+1i]
- %p = [-50 -50+20i -50-20i 0-40i 0+40i]
- % p = [-1 -1+0.5i -1-0.5i -2-1i -2+1i]
- %p = [-50 -50+20i -50-20i -30-10i -30+10i];
- %p = [-1 -1 -1.001 -1.001 -0.999];
- %p = [-5 -5 -4.999 -4.999 -5.001]
- %p = [-40 -40 -39.99 -39.99 -40.01];
-  %p = [-30 -20 -40 -25 -10];
-  %p = [-10 -10 -20-10i -20+10i -15]
-  %p = [-5.751+4.824i -5.751-4.824i -0.897 -7.637 -0.821];
-  %p = [-0.8660-0.5i -0.8660+0.5i -0.9659-0.2588i -0.9659+9.2588i -1];
-  %p = [-1 -0.97-0.26i -0.97+0.26i -0.87-0.5i -0.87+0.5i];
+
+%Pole placement
   r = 10;
   theta = 10*pi/180;
   p=[-r -r*(cos(theta)+1i*sin(theta)) -r*(cos(-theta)+1i*(sin(-theta))) -r*(cos(theta/2)+1i*(sin(theta/2))) -r*(cos(-theta/2)+1i*(sin(-theta/2)))];
